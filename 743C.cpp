020ccecb4a139d74b1994a53a8e7a13e8f82e1f5
sofdev-ms <msharmajiit@gmail.com>
2020-01-18 19:23:38 +0530

    1500

---
+++ 743C.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+
+
+
+int main()
+{
+   ll n;
+   cin >> n;
+   if( n == 1)
+   cout << "-1" << endl;
+   else
+   cout << n << " " << n + 1 << " " << n + n* n << endl;
+   
+  
+ 
+  
+}
